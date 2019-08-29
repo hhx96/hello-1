14db2c6dd78981e3513e5635fe7ffa65a52697e1
Jacob Carter <carterjayr@gmail.com>
2019-08-29 14:13:52 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Jacob Carter and Jared Switzer
+ * Date: 2019/27/08
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Let's go dude!\n");
+
+  return 0;
+}
